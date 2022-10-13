46ed08cfaffd08958883381d798d98c614aeab50
立党 Lidang <wxr001@gmail.com>
2022-10-13 17:52:04 -0500

    commit

---
+++ CodeRedDAO/SmartContract/contracts/BaseDAO.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract MyContract is Initializable {
+    uint256 public x;
+
+    function initialize(uint256 _x) public initializer {
+        x = _x;
+    }
+}
--- CodeRedDAO/SmartContract/hardhat.config.ts
+++ CodeRedDAO/SmartContract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
